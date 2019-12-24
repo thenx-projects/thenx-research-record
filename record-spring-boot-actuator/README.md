7e8f80ad2b022976bf4ee092cf1cd69c23f7a148
wei_yu <1455755530@qq.com>
2019-12-24 18:49:31 +0800

    add：调整 server模块

---
+++ record-spring-boot-actuator/README.md
@@ -0,0 +1,3 @@
+# Spring Actuator
+
+spring actuator 项目监控，这里拆分一个服务端和两个客户端
--- record-spring-boot-actuator/record-server/src/main/java/org/thenx/research/ServerApplication.java
+++ record-spring-boot-actuator/record-server/src/main/java/org/thenx/research/ServerApplication.java
@@ -1,13 +1,17 @@
-package org.thenx.record.recordspringbootactuactor;
+package org.thenx.research;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author May
+ * <p>
+ * Actuator 服务端
+ */
 @SpringBootApplication
-public class RecordSpringBootActuactorApplication {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecordSpringBootActuactorApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
-
 }
