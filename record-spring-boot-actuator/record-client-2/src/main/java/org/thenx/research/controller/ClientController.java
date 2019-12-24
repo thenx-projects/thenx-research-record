14048d69f5ef7875d7b8064003aea03116684c13
wei_yu <1455755530@qq.com>
2019-12-24 19:25:28 +0800

    add：添加客户端 2，并添加每个客户端的测试接口

---
+++ record-spring-boot-actuator/record-client-2/src/main/java/org/thenx/research/controller/ClientController.java
@@ -0,0 +1,26 @@
+package org.thenx.research.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author May
+ * <p>
+ * 添加测试接口
+ */
+@RestController
+@RequestMapping(value = "/")
+public class ClientController {
+
+    /**
+     * 客户端1 接口
+     *
+     * @return string
+     */
+    @GetMapping(value = "/")
+    public String  client2() {
+        return "Actuator 客户端 2";
+    }
+
+}
