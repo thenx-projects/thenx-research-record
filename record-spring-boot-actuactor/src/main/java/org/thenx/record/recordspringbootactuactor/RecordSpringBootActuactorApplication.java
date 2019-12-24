20adce4f4531dfa8f2171088778735e072429e76
wei_yu <1455755530@qq.com>
2019-12-24 18:25:55 +0800

    add：添加 Spring Boot Actuactor模块

---
+++ record-spring-boot-actuactor/src/main/java/org/thenx/record/recordspringbootactuactor/RecordSpringBootActuactorApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringbootactuactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordSpringBootActuactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordSpringBootActuactorApplication.class, args);
+    }
+
+}
---
+++ record-spring-boot-actuactor/src/test/java/org/thenx/record/recordspringbootactuactor/RecordSpringBootActuactorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringbootactuactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordSpringBootActuactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
