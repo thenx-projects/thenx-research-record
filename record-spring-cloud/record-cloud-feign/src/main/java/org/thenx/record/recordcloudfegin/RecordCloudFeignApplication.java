0f494d1a331945b835c6eae1c338a2a41b640bb4
wei_yu <1455755530@qq.com>
2019-12-25 16:38:14 +0800

    fix: feign子模块也作为client

--- record-spring-cloud/record-cloud-feign/src/main/java/org/thenx/record/recordcloudfegin/RecordCloudFeignApplication.java
+++ record-spring-cloud/record-cloud-feign/src/main/java/org/thenx/record/recordcloudfegin/RecordCloudFeignApplication.java
@@ -3,11 +3,16 @@ package org.thenx.record.recordcloudfegin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author May
+ *
+ * Fegin 调用服务
+ */
 @SpringBootApplication
-public class RecordCloudFeginApplication {
+public class RecordCloudFeignApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecordCloudFeginApplication.class, args);
+        SpringApplication.run(RecordCloudFeignApplication.class, args);
     }
 
 }
