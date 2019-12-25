bb4f3b038d4b99f0c16c1c68451783f54dbba143
wei_yu <1455755530@qq.com>
2019-12-25 14:51:03 +0800

    fix: 调整项目聚合方式，修复client无法注册到server问题

--- record-spring-cloud/record-cloud-client-1/src/main/java/org/thenx/record/recordcloudclient1/RecordCloudClient1Application.java
+++ record-spring-cloud/record-cloud-client-1/src/main/java/org/thenx/record/recordcloudclient1/RecordCloudClient1Application.java
@@ -2,14 +2,16 @@ package org.thenx.record.recordcloudclient1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author May
  * <p>
  * Eureka 客户端 1
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RecordCloudClient1Application {
 
