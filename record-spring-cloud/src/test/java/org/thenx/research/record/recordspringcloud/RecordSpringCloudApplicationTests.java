a3c52a0ce1ca08fa9e156df83dac70a59bf48800
wei_yu <1455755530@qq.com>
2019-12-25 09:57:15 +0800

    add：添加Spring Cloud技术模块

---
+++ record-spring-cloud/src/test/java/org/thenx/research/record/recordspringcloud/RecordSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thenx.research.record.recordspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
