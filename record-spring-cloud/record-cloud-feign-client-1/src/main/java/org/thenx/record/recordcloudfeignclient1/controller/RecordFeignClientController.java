318544393eb69bbf2fb5a1b7512273cceb88ffcf
wei_yu <1455755530@qq.com>
2019-12-25 18:05:15 +0800

    add: 服务之间的相互调用

--- record-spring-cloud/record-cloud-feign-client-1/src/main/java/org/thenx/record/recordcloudfeignclient1/controller/RecordFeignClientController.java
+++ record-spring-cloud/record-cloud-feign-client-1/src/main/java/org/thenx/record/recordcloudfeignclient1/controller/RecordFeignClientController.java
@@ -2,6 +2,7 @@ package org.thenx.record.recordcloudfeignclient1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thenx.record.recordcloudfeignclient1.service.EurekaClientService;
 import org.thenx.record.recordcloudfeignclient1.service.RecordFeignClientService;
 
 import javax.annotation.Resource;
@@ -17,13 +18,26 @@ public class RecordFeignClientController {
     @Resource
     private RecordFeignClientService recordFeignClientService;
 
+    @Resource
+    private EurekaClientService eurekaClientService;
+
     /**
-     * 试试水
+     * 调用 record-cloud-feign 对外开放的接口
      *
      * @return string
      */
     @GetMapping(value = "/client")
     public String client() {
         return recordFeignClientService.result();
+    }
+
+    /**
+     * 调用 record-cloud-client-1 对外开放的接口
+     *
+     * @return string
+     */
+    @GetMapping(value = "/resp")
+    public String resp() {
+        return eurekaClientService.resp();
     }
 }
