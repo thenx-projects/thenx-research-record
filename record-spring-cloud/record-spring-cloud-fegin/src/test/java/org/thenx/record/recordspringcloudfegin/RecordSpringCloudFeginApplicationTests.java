3366f447a0ff75d778b1a8a31ee94e88ed717011
wei_yu <1455755530@qq.com>
2019-12-25 15:51:49 +0800

    add: 添加Fegin模块

---
+++ record-spring-cloud/record-spring-cloud-fegin/src/test/java/org/thenx/record/recordspringcloudfegin/RecordSpringCloudFeginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringcloudfegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordSpringCloudFeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
