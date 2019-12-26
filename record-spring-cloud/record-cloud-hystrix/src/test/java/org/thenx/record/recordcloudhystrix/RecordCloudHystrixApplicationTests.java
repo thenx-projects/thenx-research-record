8101d43d57cb0c4ea0e9c1557cb33f32c5bd07a0
wei_yu <1455755530@qq.com>
2019-12-26 10:23:11 +0800

    add: 添加Hystrix，做服务降级、服务熔断、线程和信号隔离、请求缓存、请求合并以及服务监控

---
+++ record-spring-cloud/record-cloud-hystrix/src/test/java/org/thenx/record/recordcloudhystrix/RecordCloudHystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordcloudhystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordCloudHystrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
