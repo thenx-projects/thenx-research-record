a7918ee3fc40cae3621059699f46a2cde9e2f71e
wei_yu <admin@thenx.org>
2019-12-26 20:00:09 +0800

    add: 配置完成 Swagger接口文档

--- record-spring-boot-swagger/src/main/java/org/thenx/record/recordspringbootswagger/RecordSpringBootSwaggerApplication.java
+++ record-spring-boot-swagger/src/main/java/org/thenx/record/recordspringbootswagger/RecordSpringBootSwaggerApplication.java
@@ -2,7 +2,14 @@ package org.thenx.record.recordspringbootswagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author thenx
+ *
+ * 启动类添加开启Swagger注解
+ */
+@EnableSwagger2
 @SpringBootApplication
 public class RecordSpringBootSwaggerApplication {
 
