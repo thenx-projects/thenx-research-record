8634019653a8dacf9d3e91230fc86fb646a4bdf6
wei_yu <admin@thenx.org>
2019-12-26 19:24:06 +0800

    add: 添加 Swagger接口文档及接口测试模块

---
+++ record-spring-boot-swagger/src/main/java/org/thenx/record/recordspringbootswagger/RecordSpringBootSwaggerApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringbootswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordSpringBootSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordSpringBootSwaggerApplication.class, args);
+    }
+
+}
