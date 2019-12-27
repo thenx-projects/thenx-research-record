08ae4ca1aafe6898045a6bfaff0296aff9e6f56e
wei_yu <1455755530@qq.com>
2019-12-27 15:10:31 +0800

    add: 添加 网关模块

---
+++ record-spring-cloud/record-cloud-zuul/src/main/java/org/thenx/record/recordcloudzuul/RecordCloudZuulApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordCloudZuulApplication.class, args);
+    }
+
+}
