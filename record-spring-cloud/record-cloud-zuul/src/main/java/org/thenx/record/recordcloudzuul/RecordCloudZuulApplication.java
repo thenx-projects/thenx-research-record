2c429333a2b9a44861a80a97b9c5cb8d1ba25c30
wei_yu <1455755530@qq.com>
2019-12-27 15:51:39 +0800

    fix: zuul 网关调整及修改

--- record-spring-cloud/record-cloud-zuul/src/main/java/org/thenx/record/recordcloudzuul/RecordCloudZuulApplication.java
+++ record-spring-cloud/record-cloud-zuul/src/main/java/org/thenx/record/recordcloudzuul/RecordCloudZuulApplication.java
@@ -2,7 +2,14 @@ package org.thenx.record.recordcloudzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @author May
+ * <p>
+ * 网关操作
+ */
+@EnableZuulProxy
 @SpringBootApplication
 public class RecordCloudZuulApplication {
 
