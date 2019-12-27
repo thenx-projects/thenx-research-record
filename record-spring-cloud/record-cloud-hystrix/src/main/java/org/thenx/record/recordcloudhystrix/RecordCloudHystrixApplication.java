764d3f3babe3c52e86664599658882e271d89ef4
wei_yu <1455755530@qq.com>
2019-12-27 09:16:55 +0800

    fix: 修复 Hystrix 启动入口

--- record-spring-cloud/record-cloud-hystrix/src/main/java/org/thenx/record/recordcloudhystrix/RecordCloudHystrixApplication.java
+++ record-spring-cloud/record-cloud-hystrix/src/main/java/org/thenx/record/recordcloudhystrix/RecordCloudHystrixApplication.java
@@ -3,6 +3,11 @@ package org.thenx.record.recordcloudhystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author May
+ * <p>
+ * Hystrix
+ */
 @SpringBootApplication
 public class RecordCloudHystrixApplication {
 
