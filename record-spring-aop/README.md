2d76d4ddce354d3fc69a973d17795cbcdfe90b1c
wei_yu <admin@thenx.org>
2019-12-28 23:21:34 +0800

    fix: 启动类修改

---
+++ record-spring-aop/README.md
@@ -0,0 +1,3 @@
+# Spring Boot AOP
+
+我们可以用AOP做日志源，最终交给ES作处理
--- record-spring-aop/src/main/java/org/thenx/record/recordspringaop/RecordSpringAopApplication.java
+++ record-spring-aop/src/main/java/org/thenx/record/recordspringaop/RecordSpringAopApplication.java
@@ -2,7 +2,14 @@ package org.thenx.record.recordspringaop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author May
+ *
+ * AOP 相关操作
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class RecordSpringAopApplication {
 
