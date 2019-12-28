6d35e7ec9b0bc06f534370d6b47cbba32eeccade
wei_yu <admin@thenx.org>
2019-12-28 22:23:32 +0800

    add: 添加AOP相关依赖及服务

---
+++ record-spring-aop/src/main/java/org/thenx/record/recordspringaop/RecordSpringAopApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordSpringAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordSpringAopApplication.class, args);
+    }
+
+}
