48b6c7304a4d96f2b46a7df4743ba4e25f9ad5c7
wei_yu <admin@thenx.org>
2019-12-29 18:43:07 +0800

    fix: 添加测试Service

---
+++ record-spring-aop/src/main/java/org/thenx/record/recordspringaop/point/AspectService.java
@@ -0,0 +1,14 @@
+package org.thenx.record.recordspringaop.point;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author May
+ * <p>
+ * AOP 实现类
+ */
+@Aspect
+@Component
+public class AspectService {
+}
---
+++ record-spring-aop/src/main/java/org/thenx/record/recordspringaop/service/AopService.java
@@ -0,0 +1,15 @@
+package org.thenx.record.recordspringaop.service;
+
+/**
+ * @author May
+ * <p>
+ * 普通接口
+ */
+@FunctionalInterface
+public interface AopService {
+
+    /**
+     * 返回个接口
+     */
+    void resp();
+}
