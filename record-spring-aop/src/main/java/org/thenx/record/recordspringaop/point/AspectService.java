0ac2cbd85a5491caa0510666d2bfe894c23d69b4
wei_yu <admin@thenx.org>
2019-12-29 19:08:25 +0800

    fix: 调整每个过程切点

--- record-spring-aop/src/main/java/org/thenx/record/recordspringaop/point/AspectService.java
+++ record-spring-aop/src/main/java/org/thenx/record/recordspringaop/point/AspectService.java
@@ -18,42 +18,66 @@ public class AspectService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Pointcut(value = "execution(* org.thenx.record.recordspringaop.controller..*.*(..))")
+    @Pointcut(value = "execution(* org.thenx..*.*(..))")
     public void pointcut() {
         logger.info("\n-------> 1. 进入 @Pointcut方法");
     }
 
-    @Before(value = "pointcut()")
-    public void beginTransaction() {
-        logger.info("\n-------> 2. 进入 @Before 方法");
-    }
-
-    @After(value = "pointcut()")
-    public void commit() {
-        logger.info("\n-------> 3. 进入 @After 方法");
-    }
-
-    @AfterReturning(value = "pointcut()", returning = "returnObject")
-    public void afterReturning(JoinPoint joinPoint, Object returnObject) {
-        logger.info("\n-------> 4. 进入 @AfterReturning 方法" + " JoinPoint: " + joinPoint + " & returnObject: " + returnObject);
-    }
-
-    @AfterThrowing(value = "pointcut()")
-    public void afterThrowing() {
-        logger.info("\n-------> 5. 进入 @AfterThrowing 方法，异常可回滚");
-    }
-
+    /**
+     *
+     * 第一个进入
+     *
+     * @param joinPoint join point
+     * @return obj
+     * @throws Throwable thr
+     */
     @Around(value = "pointcut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         logger.info("\n-------> 6. 进入 @Around 方法" + " ProceedingJoinPoint: " + joinPoint);
         try {
-            System.out.println("around");
+            logger.info("\n-------> try 进入 Around 方法");
             return joinPoint.proceed();
         } catch (Throwable e) {
             e.printStackTrace();
             throw e;
         } finally {
-            System.out.println("around");
+            logger.info("\n-------> finally 进入 Around 方法");
         }
     }
+
+    /**
+     * 第二个进入
+     */
+    @Before(value = "pointcut()")
+    public void beginTransaction() {
+        logger.info("\n-------> 2. 进入 @Before 方法");
+    }
+
+    /**
+     * 第三个进入
+     */
+    @After(value = "pointcut()")
+    public void commit() {
+        logger.info("\n-------> 3. 进入 @After 方法");
+    }
+
+    /**
+     * 第四个进入
+     * @param joinPoint join point
+     * @param returnObject obj
+     */
+    @AfterReturning(value = "pointcut()", returning = "returnObject")
+    public void afterReturning(JoinPoint joinPoint, Object returnObject) {
+        logger.info("\n-------> 4. 进入 @AfterReturning 方法" + " JoinPoint: " + joinPoint + " & returnObject: " + returnObject);
+    }
+
+    /**
+     * 有异常的时候进入
+     */
+    @AfterThrowing(value = "pointcut()")
+    public void afterThrowing() {
+        logger.info("\n-------> 5. 进入 @AfterThrowing 方法，异常可回滚");
+    }
+
+
 }
