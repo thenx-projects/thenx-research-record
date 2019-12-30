4011fb155c7249b1edef02c6fc8233f2c74ec59f
wei_yu <1455755530@qq.com>
2019-12-30 18:49:06 +0800

    add: 添加 Redis 模块

---
+++ record-spring-boot-redis/src/main/java/org/thenx/record/recordspringbootredis/RecordSpringBootRedisApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringbootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordSpringBootRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordSpringBootRedisApplication.class, args);
+    }
+
+}
