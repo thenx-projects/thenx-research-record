9a02b592df0c414383155a7458992886135b4bf0
wei_yu <1455755530@qq.com>
2020-01-02 17:44:41 +0800

    add:  添加 Spring Security 模块功能

---
+++ record-spring-security/src/main/java/org/thenx/record/recordspringsecurity/RecordSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ record-spring-security/src/test/java/org/thenx/record/recordspringsecurity/RecordSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
