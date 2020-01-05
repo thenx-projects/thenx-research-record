d8092b6812ea22d1418e292362e5e76b365a2bbb
wei_yu <admin@thenx.org>
2020-01-05 17:28:26 +0800

    add: 添加 Security 配置相关

---
+++ record-spring-security/src/main/java/org/thenx/record/recordspringsecurity/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.thenx.record.recordspringsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author May
+ * <p>
+ * 配置 Spring Security
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 配置用户、权限相关
+     *
+     * @param auth auth
+     * @throws Exception exc
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    /**
+     * 配置拦截规则
+     *
+     * @param http http
+     * @throws Exception exc
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
