98834a9de4b5f04ac86445d12da6bd00b46af586
wei_yu <thenx-wei@thenx.org>
2020-01-24 16:28:26 +0800

    add: 集成 kafka

---
+++ record-kafka/src/main/java/org/thenx/record/recordkafka/RecordKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.thenx.record.recordkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordKafkaApplication.class, args);
+    }
+
+}
