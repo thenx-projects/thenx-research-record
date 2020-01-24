8040ced33d08babb221299362a363e4c65865c0a
wei_yu <admin@thenx.org>
2020-01-24 16:01:34 +0800

    refactoring: 去掉移除后的残余注册中心

--- record-spring-aop/src/main/java/org/thenx/record/recordspringaop/RecordSpringAopApplication.java
+++ record-spring-aop/src/main/java/org/thenx/record/recordspringaop/RecordSpringAopApplication.java
@@ -2,14 +2,12 @@ package org.thenx.record.recordspringaop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author May
  *
  * AOP 相关操作
  */
-@EnableEurekaClient
 @SpringBootApplication
 public class RecordSpringAopApplication {
 
--- record-spring-security/src/main/java/org/thenx/record/recordspringsecurity/RecordSpringSecurityApplication.java
+++ record-spring-security/src/main/java/org/thenx/record/recordspringsecurity/RecordSpringSecurityApplication.java
@@ -2,15 +2,12 @@ package org.thenx.record.recordspringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author May
  * <p>
  * Security 权限控制
  */
-@EnableEurekaClient
 @SpringBootApplication
 public class RecordSpringSecurityApplication {
 
