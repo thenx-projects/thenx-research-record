6cc64d87829beaf81ea4919eaa4280075ddb145c
wei_yu <thenx-wei@thenx.org>
2020-01-25 19:56:59 +0800

    fix: 添加 kafka生产者

---
+++ record-kafka/src/main/java/org/thenx/record/recordkafka/producer/KafkaProducer.java
@@ -0,0 +1,50 @@
+package org.thenx.record.recordkafka.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.lang.NonNullApi;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import javax.annotation.Resource;
+
+/**
+ * @author thenx
+ * <p>
+ * kafka 消息生产者
+ */
+@Component
+public class KafkaProducer {
+
+    @Resource
+    private KafkaTemplate kafkaTemplate;
+
+    /**
+     * 消息发送
+     *
+     * @param topic  topic
+     * @param object msg
+     */
+    public void sendMsg(String topic, Object object) {
+
+        /*
+         * 这里的ListenableFuture类是spring对java原生Future的扩展增强,是一个泛型接口,用于监听异步方法的回调
+         * 而对于kafka send 方法返回值而言，这里的泛型所代表的实际类型就是 SendResult<K, V>,而这里K,V的泛型实际上
+         * 被用于ProducerRecord<K, V> producerRecord,即生产者发送消息的key,value 类型
+         */
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(topic, object);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println("----------> 消息发送失败: " + throwable.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> stringObjectSendResult) {
+                System.out.println("----------> 消息发送成功： " + stringObjectSendResult.toString());
+            }
+        });
+
+    }
+}
