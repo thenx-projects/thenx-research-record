2940f84fb504d51b39f0a3bb3b823564883ee1ec
wei_yu <thenx-wei@thenx.org>
2020-03-29 17:47:43 +0800

    fix: 修复 consumer配置文件

---
+++ record-kafka/kafka-producer/src/main/java/org/thenx/kafka/producer/service/KafkaProducer.java
@@ -0,0 +1,38 @@
+package org.thenx.kafka.producer.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import javax.annotation.Resource;
+
+/**
+ * @author thenx
+ * <p>
+ * Kafka Producer
+ */
+@Slf4j
+@Configuration
+public class KafkaProducer {
+
+    @Resource
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMsg(String topic, Object object) {
+        ListenableFuture<SendResult<String, Object>> send = kafkaTemplate.send(topic, object);
+        send.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.error("消息发送失败： {}", throwable.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> stringObjectSendResult) {
+                log.info("消息发送成功： {}", stringObjectSendResult.toString());
+            }
+        });
+    }
+}
