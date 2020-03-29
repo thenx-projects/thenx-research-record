1a9491d686325677e29dc79c94f6a5463ad5b2f2
wei_yu <thenx-wei@thenx.org>
2020-03-29 14:00:20 +0800

    add: 重新添加kafka模块

---
+++ record-kafka/src/test/java/org/thenx/record/kafka/RecordKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thenx.record.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
