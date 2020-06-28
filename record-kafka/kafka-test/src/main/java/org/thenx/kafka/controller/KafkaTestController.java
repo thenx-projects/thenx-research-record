5ff73e416f0429a80f070320a09e62a80442e609
thenx-wei <thenx-wei@thenx.org>
2020-06-28 14:57:04 +0800

    fixed and created: basics

--- record-kafka/kafka-test/src/main/java/org/thenx/kafka/controller/KafkaTestController.java
+++ record-kafka/kafka-test/src/main/java/org/thenx/kafka/controller/KafkaTestController.java
@@ -1,16 +1,15 @@
 package org.thenx.kafka.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.thenx.kafka.entity.KafkaTestEntity;
 import org.thenx.kafka.producer.service.KafkaProducer;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author thenx
@@ -24,35 +23,12 @@ public class KafkaTestController {
     @Resource
     private KafkaProducer kafkaProducer;
 
-    @SneakyThrows
     @GetMapping(value = "/")
     public Object testKafka(@RequestParam String msg) {
-        ObjectMapper mapper = new ObjectMapper();
         String data = msg + UUID.randomUUID().toString();
-        KafkaTestEntity kafkaTestEntity = this.get();
-
-        List<KafkaTestEntity> list = new ArrayList<>();
-        list.add(kafkaTestEntity);
-        list.add(kafkaTestEntity);
-        list.add(kafkaTestEntity);
-
-        Map<String, Object> map1 = new HashMap<>(6);
-        map1.put("1", kafkaTestEntity);
-        map1.put("2", kafkaTestEntity);
-        map1.put("3", kafkaTestEntity);
-
-        String s = mapper.writeValueAsString(map1);
-        kafkaProducer.sendMsg("topic02", s);
+        kafkaProducer.sendMsg("topic02", data);
         Map<String, String> map = new HashMap<>(4);
         map.put("topic02", data);
         return map;
-    }
-
-    private KafkaTestEntity get() {
-        KafkaTestEntity kafkaTestEntity = new KafkaTestEntity();
-        kafkaTestEntity.setId(UUID.randomUUID().toString().replace("-", ""));
-        kafkaTestEntity.setUserName("kafkaEntity Name");
-        kafkaTestEntity.setPwd("1234");
-        return kafkaTestEntity;
     }
 }
