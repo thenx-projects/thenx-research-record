051d3501853c6bdcc767f1a072a2d7faf8fc0fc2
thenx-wei <thenx-wei@thenx.org>
2020-07-07 11:21:19 +0800

    fixed: 添加关联

--- record-basics/basics-rle/src/main/java/org/thenx/rle/RleA.java
+++ record-basics/basics-rle/src/main/java/org/thenx/rle/RleA.java
@@ -3,9 +3,10 @@ package org.thenx.rle;
 public class RleA {
 
     public static void main(String[] args) {
-        String source = "212611417117726053643435767636171750066066071013";
+        String source = "110000011010000000001011101100110010100010001001000111001100001110101010001000110001010010111011001110100000101110110011001000100011000000111011101010011100001000111001101100101000101001001";
+        System.out.println("source length: " + source.length());
         String s = rleCompress(source);
-        System.out.println("---> rle compression: " + s);
+        System.out.println("---> rle compression: " + s + " \n&length: " + s.length());
     }
 
     public static String rleCompress(String source) {
